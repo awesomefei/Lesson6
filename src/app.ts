5d31cc5bfd0bbe52a99f2ad0b1f8d530d592c7dc
Ann <lifeiwanguk@gmail.com>
2017-03-06 15:03:08 -0800

    Fields vs Properties

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,55 @@
-console.log('hello world');
+// namespace MathUtility{
+//    export class Calculator{
+//         public add(a,b){
+//             return a + b;
+//         }
+//         public substract(a,b){
+//             return a-b;
+//         }
+//         public multiply(a,b){
+//             return a *b;
+//         }
+//         public divide(a,b){
+//             if(b == 0){
+//                 return 'err';
+//             }else{
+//                 return a/b;
+//             }
+//         }
+//     }
+// }
+// namespace MyApp{
+//     let cal = new MathUtility.Calculator(); 
+//     console.log(cal.divide(1,2));
+// }
+
+
+class Customer{
+    private _firstName : string;
+    private _lastName : string; 
+    constructor(firstName:string, lastName:string){
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+    get FirstName(){
+        return this._firstName;
+    }
+    get LastName(){
+        return this._lastName;
+    }
+    set FirstName(firstName:string){
+        if(firstName == ''|| null){
+             throw new Error('firstName should be empty!');
+        }
+        this._firstName = firstName;
+    }
+    set LastName(lastName:string){
+        if(lastName == null){
+             throw new Error('lastName should be empty!');
+        }
+        this._lastName = lastName;
+    }
+
+}
+let c = new Customer('', 'wang');
+console.log(c.FirstName);
